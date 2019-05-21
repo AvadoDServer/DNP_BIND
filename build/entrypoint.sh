68b9df6f85d0228d8e26de0fdddab24e562fa595
vdo <vdo@greyfaze.net>
2019-05-21 20:14:38 +0200

    Add dappnode.io to redirect

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -7,6 +7,11 @@ if [ ! -f /etc/bind/dappnode.hosts ]; then
     cp /config/dappnode.hosts /etc/bind/dappnode.hosts
 fi
 
+if [ ! -f /etc/bind/dappnode.io.hosts ]; then
+    cp /config/dappnode.io.hosts /etc/bind/dappnode.io.hosts
+fi
+
+
 diff /etc/bind/named.conf /config/named.conf
 if [ $? -ne 0 ]; then
     cp /config/named.conf /etc/bind/named.conf
